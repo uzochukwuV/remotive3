13f5c3ac158e8a369d96389c851f6db0d91b630a
uzochukwuv <vic.ezealor@gmail.com.com>
2025-03-19 11:10:52 +0100

    basic template

--- contracts/Payroll.sol
+++ contracts/Payroll.sol
@@ -3,5 +3,42 @@ pragma solidity ^0.8.28;
 
 // Uncomment this line to use console.log
 // import "hardhat/console.sol";
+import {IERC20} from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
+import {ReentrancyGuard} from "openzeppelin-contracts/contracts/utils/ReentrancyGuard.sol";
 
-contract Payroll {}
+contract Payroll {
+    error InvalidAddress();
+    error InvalidAmount();
+    error InvalidDuration();
+    error InsufficientBalance();
+    error JobNotFound();
+    error JobDone();
+    error Unauthorized();
+
+    struct Job {
+        address from;
+        address to;
+        address token;
+        uint256 depositAmount;
+        uint256 amountPerMileStone;
+        uint256 numOfMileStone;
+        // case should we do it by mile stone or by timeframe
+        uint256 remainingBalance;
+        uint256 lastUpdate;
+    }
+    mapping(uint256 => Job) public jobs;
+    uint256 public jobId;
+    // tracks num of jobs and provides unique ids for each
+
+    // events here ...
+
+     modifier onlyValidAddress(address _address) {
+        if (_address == address(0) || _address == address(this)) {
+            revert InvalidAddress();
+        }
+        _;
+    }
+
+    
+}
---
+++ contracts/interfaces/IPayroll.sol
@@ -0,0 +1,6 @@
+
+// will contain all functions in payroll contract
+interface IPayroll {
+    function createJob();
+    function addEmployeee();
+}
